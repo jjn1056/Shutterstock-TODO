c3c19273a18070bf554fc34b46bc073009c6c183
John Napiorkowski <jjn1056@yahoo.com>
2012-09-17 20:37:06 -0400

    basics

---
+++ lib/Shutterstock/TODO.pm
@@ -0,0 +1,39 @@
+package Shutterstock::TODO;
+
+1;
+
+=head1 NAME
+
+Shutterstock::TODO - A cool example of a TODO application
+
+=head1 SYNOPSIS
+
+    TBD
+
+=head1 DESCRIPTION
+
+This is a sample application designed to demo L<DBIx::Class> and related
+technologies.
+
+=head1 STORIES
+
+see L<Shutterstock::Stories>
+
+=head1 SEE ALSO
+
+The following modules or resources may be of interest.
+
+L<DBIx::Class>
+
+=head1 AUTHOR
+
+    John Napiorkowski C<< <jjnapiork@cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+    Copyright 2012, John Napiorkowski C<< <jjnapiork@cpan.org> >>
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
