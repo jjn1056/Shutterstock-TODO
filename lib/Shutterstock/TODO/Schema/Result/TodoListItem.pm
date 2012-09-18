b9f560c16880725a5f1e86d96dc62ef3dfaa144f
John Napiorkowski <jjn1056@yahoo.com>
2012-09-18 14:04:52 -0400

    model correction

---
+++ lib/Shutterstock/TODO/Schema/Result/TodoListItem.pm
@@ -0,0 +1,31 @@
+package Shutterstock::TODO::Schema::Result::TodoListItem;
+
+use Shutterstock::TODO::Schema::Candy;
+
+table 'todo_list';
+
+1;
+
+=head1 NAME
+
+Shutterstock::TODO::Schema::Result::TodoListItem
+
+=head1 DESCRIPTION
+
+    TBD
+
+=head1 METHODS
+
+    TBD
+
+=head1 AUTHOR
+
+See L<Shutterstock::TODO> for authorship information.
+
+=head1 COPYRIGHT & LICENSE
+
+See L<Shutterstock::TODO> for rights information.
+
+=cut
+
+
--- t/basic.t
+++ t/basic.t
@@ -1,6 +1,6 @@
 use Test::Most;
 use Test::DBIx::Class
   -schema_class=>'Shutterstock::TODO::Schema',
-  qw(Person TodoList);
+  qw(Person);
 
 done_testing;
